ae4d91aba32ebc34f312dd14f234b460bbc88c29
kelwing <kelwing@kelnet.org>
2025-11-15 10:35:53 -0500

    feat: another key

--- users.nix
+++ users.nix
@@ -7,6 +7,7 @@
       extraGroups = [ "wheel" ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFjnOwCitFWrv7GeMnqIsuRAj/oA2B8ehrtqjCkFnqBg kelwing@desktop"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJgW0mNhzaFegPBdf35nTks7bZVJQLMYh5ZgpA1V2GGb kelwing@mbp"
       ];
     };
   };
